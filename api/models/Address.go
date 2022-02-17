c9348d3552a46036585da2770b37f784f0b1864d
Wesley Moizaque <wesley.moizaque@fretebras.com.br>
2022-02-17 19:39:02 -0300

    fix: changed the type

--- api/models/Address.go
+++ api/models/Address.go
@@ -8,8 +8,8 @@ type Address struct {
 	ID         uint64    `gorm:"primary_key;auto_increment" json:"id"`
 	Address    string    `gorm:"size:255;not null" json:"address"`
 	PostalCode string    `gorm:"size:255;not null" json:"postal_code"`
-	Latitude   string    `gorm:"not null" json:"latitude"`
-	Longitude  string    `gorm:"not null" json:"longitude"`
+	Latitude   float64    `gorm:"not null" json:"latitude"`
+	Longitude  float64    `gorm:"not null" json:"longitude"`
 	CreatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt  time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
 }
